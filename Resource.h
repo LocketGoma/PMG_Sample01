640c56736e6217e1e5a74967094b8d594bd20a8b
LocketGoma <LocketGoma@users.noreply.github.com>
2020-01-10 00:59:04 +0900

    코딱지만큼 주석 업데이트

--- Resource.h
+++ Resource.h
@@ -13,11 +13,9 @@
 #define MIN_LENGTH 3				//길이 최소값
 #define MAX_LENGTH 7				//길이 최대값
 #define MAX_Rooms 51				//최대 방 개수
-#define MAX_TRY MAX_AXIS*MAX_AXIS
+#define MAX_TRY MAX_AXIS*MAX_AXIS	//최대 시도 회수 (= 좌표 최대 크기의 제곱)
 
 using namespace std;
-
-
 
 
 
@@ -30,7 +28,7 @@ using namespace std;
 /* 구현할것
 1. 방 생성 후 방을 Node로 삼아 Tree구조로 재배치
 2. Tree구조로 배치된 기준하여 방 연결
-
+3. 'JSON 양식'으로 출력시키기.
 
 */
 
